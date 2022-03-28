49809c1362b744839edee3e584b372c86d3dfcfe
codejks <goldjks0512@gmail.com>
2022-03-28 09:18:30 +0900

    css mod

--- src/Container/Map.tsx
+++ src/Container/Map.tsx
@@ -3,22 +3,22 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 
 export default function Map() {
     
-    const [lat, setLat] = useState(37.401579);
-    const [lng, setLng] = useState(126.966115);
+    // const [lat, setLat] = useState(0);
+    // const [lng, setLng] = useState(0);
 
-    navigator.geolocation.getCurrentPosition(function(position){
+    function location() {navigator.geolocation.getCurrentPosition(function(position){
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
-        console.log(lat);
-        console.log(lng);
-    })
-    
+        return [lat,lng];
+        })
+    }
+
     return (
         <div className="Map">
             <MapContainer
             style={{position:'absolute', width:'100%', height:'100vh', zIndex:-10}}
             // center={[37.401586,126.966043]}
-            center={[lat,lng]}
+            center={location}
             zoom={16}
             zoomControl={false}
             dragging={true}
